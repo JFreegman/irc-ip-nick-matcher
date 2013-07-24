baa1e0e9a6f25e172221962f0d5098ef963b9cf3
Stuart Banks <Jfreegman@gmail.com>
2013-07-24 03:33:42 -0400

    Minor tweaks and added license

--- ipmatcher.py
+++ ipmatcher.py
@@ -3,14 +3,15 @@
 # irc-ip-nick-matcher 
 # version: 0.1
 # Author: Stuart Banks (JFreegman@gmail.com)
+# License: GPLv3
 
 import re
 import sys
 
 LOGPATHFILE = open("logpath", 'r')
 
-def match_IP(ipCheck):
-    PATH = open("".join(L for L in LOGPATHFILE).strip())
+def match_IP(IP_Check):
+    PATH = open("".join(L for L in LOGPATHFILE).strip(), 'r')
 
     # find lines from log in which a user's IP is displayed
     IP_Lines = [line.strip() for line in PATH if re.search(r'( \*.* \(.*\@.*\))', line)]
@@ -20,7 +21,7 @@ def match_IP(ipCheck):
     for line in IP_Lines:
         start, end = line.find('@')+1, line.find(')')
         IP = line[start:end]
-        if IP in ipCheck:
+        if IP in IP_Check:
             start, end = line.find('*')+2, line.find('(')-1
             nick = line[start:end]
             if nick[0] in '@+%':    # remove special chars from nick
@@ -28,9 +29,9 @@ def match_IP(ipCheck):
             nickMatches.add(nick)
 
     if not nickMatches:
-        print "No matches for: " + ipCheck
+        print "No matches for: " + IP_Check
     else:
-        print 'Nick matches for ' + ipCheck + ':'
+        print 'Nick matches for ' + IP_Check + ':'
         for nick in nickMatches:
             print nick
 
